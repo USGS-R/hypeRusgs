c186f659089918a1da200c7bfb0c58e6ea8c8303
Platt <lplatt@usgs.gov>
2019-06-25 14:29:08 -0500

    updates to 6-26 with plotly examples

--- Webinar-6-26-2019/3d_plots.R
+++ Webinar-6-26-2019/3d_plots.R
@@ -1,25 +1,45 @@
 # 3-D plots
 
 library(raster)
+library(plotly)
 library(ggplot2)
-library(rayshader)
+library(rayshader) # might need to install with `devtools::install_github("tylermorganwall/rayshader")`
 
-# Plotly has an offline capability, but I don't know how you 
-# can get the plots without publishing publicly or having 
-# an enterprise server. See https://plot.ly/r/offline/
-
-# Load surface elevation data
+##### Load surface elevation data #####
 surface <- raster("Webinar-6-26-2019/foxcanyon_aq/w001001.adf")
 
-# You can get 3D elevation plots using the package rayshader & ggplot
-# See https://github.com/tylermorganwall/rayshader for more information
-
+# As data.frame for ggplot2
 surface_plot_ready <- as.data.frame(as(surface, "SpatialPixelsDataFrame"))
+
+# As matrix for plotly and rayshader
+surface_matrix <- matrix(raster::extract(surface,raster::extent(surface),buffer=1000),
+                         nrow=ncol(surface),ncol=nrow(surface))
+
+# Create a 2D surface elevation plot
 surface_ggplot <- ggplot(surface_plot_ready,
                          aes(x=x, y=y, fill = w001001)) +
   geom_raster() +
   coord_equal() +
   theme_minimal()
+
+##### Make a plotly plot locally #####
+
+surface_plotly <- plot_ly(z = ~surface_matrix, type = "surface")
+
+# Export the plotly file
+# FYI, "saveWidget" doesn't work when saving anywhere but the current working directory
+# See the following for workarounds:
+#   1. Code to wrap around the filepath: https://stackoverflow.com/a/45206860
+#   2. A custom fix function: https://github.com/ramnathv/htmlwidgets/issues/299
+htmlwidgets::saveWidget(as_widget(surface_plotly), "plotly_example.html")
+
+# Can also take an existing ggplot and convert to plotly
+ggplotly(surface_ggplot)
+
+##### Alternatives to plotly #####
+
+# You can get 3D elevation plots using the package rayshader & ggplot
+# See https://github.com/tylermorganwall/rayshader for more information
 
 # Take the 2D surface plot and convert to 3D
 plot_gg(surface_ggplot, width = 5, height = 5, scale = 250, 
@@ -29,12 +49,17 @@ plot_gg(surface_ggplot, width = 5, height = 5, scale = 250,
 rgl::clear3d()
 
 # Can do a surface plot this way too:
+zscale <- 50
+ambmat <- ambient_shade(surface_matrix, zscale = zscale)
+raymat <- ray_shade(surface_matrix, zscale = zscale, lambert = TRUE)
 
-elmat <- matrix(raster::extract(surface,raster::extent(surface),buffer=1000),
-               nrow=ncol(surface),ncol=nrow(surface))
-elmat %>%
+surface_matrix %>%
   sphere_shade(texture = "imhof1") %>%
-  plot_3d(elmat,zscale=50,fov=0,theta=-100,phi=30, zoom=0.5)
+  add_shadow(ambmat) %>%
+  add_shadow(raymat) %>%
+  plot_3d(surface_matrix, zscale = zscale, 
+          # Position the initial view perspective
+          fov = 0, theta = -100, phi = 30, zoom = 0.5)
 
 # Make a 360 view movie of the current 3d surface
 render_movie("Webinar-6-26-2019/foxcanyon.mp4")
